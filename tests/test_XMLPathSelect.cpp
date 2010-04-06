453f9911ad4ade090a5ef5fcefe51de1d159976b
Patrick Frey <patrickpfrey@yahoo.com>
2010-04-07 00:53:55 +0200

    first commit

---
+++ tests/test_XMLPathSelect.cpp
@@ -0,0 +1,51 @@
+#include "textwolf.hpp"
+#include <iostream>
+#include <map>
+
+//GCC
+//compile: g++ -c -o test_XMLPathSelect.o -g -fstrict-aliasing -pedantic -Wall -Wunused -Wno-import -Wformat -Wformat-y2k -Wformat-nonliteral -Wformat-security -Wformat-y2k -Wswitch-enum -Wunknown-pragmas -Wfloat-equal -Wundef -Wshadow -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wmissing-noreturn -Wno-multichar -Wparentheses -Wredundant-decls -Winline -Wdisabled-optimization -Wno-long-long -Werror -Wfatal-errors -I../include test_XMLPathSelect.cpp
+//link     g++ -lc -o test_XMLPathSelect test_XMLPathSelect.o
+
+using namespace textwolf;
+
+static const char* simpleXmple = "<?xml charset=isolatin-1?><note id=1 t=2 g=\"zu\"><stag value='500'/> \n<to>Frog</to>\n<from>Bird</from><body>Hello world!</body>\n</note>";
+
+int main( int, const char**)
+{ 
+   try 
+   {
+      char* src = const_cast<char*>( simpleXmple);
+      typedef XMLPathSelectAutomaton<charset::UTF8> Automaton;
+      Automaton atm;
+      (*atm)["stag"]("value","500")();
+      (*atm)["note"]("id");
+      (*atm)["note"]("id", "1");
+      (*atm)--["stag"]();
+
+      enum {outputBufSize=4096};
+      char outputBuf[ outputBufSize];
+
+      typedef XMLPathSelect<char*> MyXMLPathSelect; 
+      MyXMLPathSelect xs( &atm, src, outputBuf, outputBufSize);
+
+      MyXMLPathSelect::iterator itr,end;
+      for (itr=xs.begin(),end=xs.end(); itr!=end; itr++)
+      {         
+         if (itr->error)
+         {
+            std::cerr << "FAILED " << itr->content << std::endl;
+            exit( 1);
+         }
+         std::cout << "Element " << itr->type << ": " << std::string( itr->content, itr->size);
+      }
+      
+      std::cerr << "OK" << std::endl;
+      exit( 0);
+   }
+   catch (Exception ee)
+   {
+      std::cerr << "ERROR " << ee.msg << std::endl;
+      exit( 1);
+   };
+}
+
---
+++ tests/test_XMLScanner.cpp
@@ -0,0 +1,78 @@
+#include "textwolf.hpp"
+#include <iostream>
+#include <map>
+
+//GCC
+//compile: g++ -c -o test_XMLScanner.o -g -fstrict-aliasing -pedantic -Wall -Wunused -Wno-import -Wformat -Wformat-y2k -Wformat-nonliteral -Wformat-security -Wformat-y2k -Wswitch-enum -Wunknown-pragmas -Wfloat-equal -Wundef -Wshadow -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wmissing-noreturn -Wno-multichar -Wparentheses -Wredundant-decls -Winline -Wdisabled-optimization -Wno-long-long -Werror -Wfatal-errors -I../include test_XMLScanner.cpp
+//link     g++ -lc -o test_XMLScanner test_XMLScanner.o
+
+using namespace textwolf;
+
+static const char* simpleXmple = "<?xml charset=isolatin-1?><note id=1 t=2 g=\"zu\"><stag value='500'/> \n<to>Frog</to>\n<from>Bird</from><body>Hello world!</body>\n</note>";
+
+template <class InputCharSet, class OutputCharSet>
+struct XMLScannerTest
+{
+   typedef XMLScanner<char*,InputCharSet,OutputCharSet> MyXMLScanner;
+   enum {outputBufSize=2048};
+   char outputBuf[ outputBufSize];
+   MyXMLScanner xs;
+   
+   XMLScannerTest( char* text) :xs(text,outputBuf,outputBufSize) {};
+
+   bool run()
+   {
+      try
+      {
+         typename MyXMLScanner::iterator itr;
+         typename MyXMLScanner::iterator end;
+         for (itr=xs.begin(),end=xs.end(); itr!=end; itr++)
+         {
+            cout << "Element " << itr->name() << ": " << itr->content;
+            if (itr->type == MyXMLScanner::ErrorOccurred) break;
+         }
+         return true;
+      }
+      catch (Exception ee)
+      {
+         std::cerr << "ERROR " << ee.msg << std::endl;
+         exit( 1);
+      };
+   };
+};
+
+static bool simpleTest( const char* txt)
+{
+   typedef XMLScannerTest<CharSet_IsoLatin1,CharSet_IsoLatin1> Test;
+   return Test( const_cast<char*>(txt)).run();
+}
+
+static bool testAll()
+{
+   if (!simpleTest( simpleXmple)) return false;
+   return true;
+}
+
+int main( int, const char**)
+{   
+   bool res = testAll();
+   typedef XMLPathSelect<> PathSelect;
+   PathSelect xs;
+   PathSelect::PathElement e = xs.root();
+   e["tag1"]["tag2"]("a","v")--("a")[1];
+
+   if (res)
+   {
+      std::cerr << "OK" << std::endl;
+      exit( 0);
+   }
+   else
+   {
+      std::cerr << "ERROR" << std::endl;
+      exit( 1);
+   }
+}
+
+
+
+
