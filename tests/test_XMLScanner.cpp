24a46474ea434d2ec23e5a7832fe49f55c6fcb89
Patrick Frey <patrickpfrey@yahoo.com>
2011-06-14 01:29:10 -0700

    use std::string as buffer

--- tests/test_XMLScanner.cpp
+++ tests/test_XMLScanner.cpp
@@ -6,14 +6,13 @@
 //compile: g++ -c -o test_XMLScanner.o -g -I../include -fstrict-aliasing -pedantic -Wall -Wunused -Wno-import -Wformat -Wformat-y2k -Wformat-nonliteral -Wformat-security -Wformat-y2k -Wswitch-enum -Wunknown-pragmas -Wfloat-equal -Wundef -Wshadow -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wmissing-noreturn -Wno-multichar -Wparentheses -Wredundant-decls -Winline -Wdisabled-optimization -Wno-long-long -Werror -Wfatal-errors test_XMLScanner.cpp
 //link	  g++ -lc -o test_XMLScanner test_XMLScanner.o
 
-typedef textwolf::Buffer BufferType;
 using namespace textwolf;
 
 int main( int, const char**)
 {
 	static const char* xmlstr = "<?xml charset=isolatin-1?><note id=1 t=2 g=\"zu\"><stag value='500'/> \n<to>Frog</to>\n<from>Bird</from><body>Hello world!</body>\n</note>";
-	typedef XMLScanner<char*,charset::IsoLatin1,charset::IsoLatin1,BufferType> MyXMLScanner;
-	MyXMLScanner::OutputBuffer outputbuf(1000);
+	typedef XMLScanner<char*,charset::IsoLatin1,charset::IsoLatin1,std::string> MyXMLScanner;
+	std::string outputbuf;
 	char* xmlitr = const_cast<char*>(xmlstr);
 
 	MyXMLScanner xs( xmlitr, outputbuf);
